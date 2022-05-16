d8b59147ff13a6c18f1935172ed32d82cfd9cd1a
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-16 17:29:09 +0700

    Update EditProfileActivity.kt

--- app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
@@ -1,8 +1,16 @@
 package com.yulius.warasapp.ui.profile.editprofile
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yulius.warasapp.databinding.ActivityEditProfileBinding
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.yulius.warasapp.R
+import com.yulius.warasapp.databinding.ActivityEditProfileBin
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -12,7 +20,29 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        binding.btnGallery.setOnClickListener {
+            startGallery()
+        }
+    }
+    
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
 
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@EditProfileActivity)
+            binding.imgPhoto.setImageURI(selectedImg)
+        }
     }
 
 }
