e08860c506e3853fdf9fa0420f92597d9e9b1bc7
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-17 13:37:26 +0700

    Update EditProfileActivity.kt

--- app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
@@ -1,19 +1,21 @@
 package com.yulius.warasapp.ui.profile.editprofile
 
-import android.Manifest
-import android.app.DatePickerDialog
-import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.DatePicker
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.yulius.warasapp.R
 import com.yulius.warasapp.databinding.ActivityEditProfileBinding
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,6 +23,28 @@ class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
     var calender = Calendar.getInstance()
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.no_permission),
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +75,17 @@ class EditProfileActivity : AppCompatActivity() {
         binding.btnGallery.setOnClickListener {
             startGallery()
         }
+        
+        binding.btnCamera.setOnClickListener {
+            if (!allPermissionsGranted()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    REQUIRED_PERMISSIONS,
+                    REQUEST_CODE_PERMISSIONS
+                )
+            }
+            startTakePhoto()
+        }
     }
     
     private fun updateDateInView() {
@@ -66,6 +101,22 @@ class EditProfileActivity : AppCompatActivity() {
         val chooser = Intent.createChooser(intent, getString(R.string.choose))
         launcherIntentGallery.launch(chooser)
     }
+    
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+        createTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@EditProfileActivity,
+                "com.yulius.warasapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+
+    }
 
 
     private val launcherIntentGallery = registerForActivityResult(
@@ -76,6 +127,24 @@ class EditProfileActivity : AppCompatActivity() {
             val myFile = uriToFile(selectedImg, this@EditProfileActivity)
             binding.imgPhoto.setImageURI(selectedImg)
         }
+    }
+    
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+
+            val result = BitmapFactory.decodeFile(myFile.path)
+
+            binding.imgPhoto.setImageBitmap(result)
+        }
+    }
+    
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 
 }
