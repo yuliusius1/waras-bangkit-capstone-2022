7634c7b460404fe93e9ab238caafb901f50f4f73
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-17 12:54:40 +0700

    Add files via upload

---
+++ app/src/main/java/com/yulius/warasapp/ui/profile/setting/SettingActivity.kt
@@ -0,0 +1,45 @@
+package com.yulius.warasapp.ui.profile.setting
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.yulius.warasapp.databinding.ActivitySettingBinding
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val switchTheme = binding.switchTheme
+
+        val pref = SettingPreferences.getInstance(dataStore)
+        val mainViewModel = ViewModelProvider(this, SettingViewModelFactory(pref)).get(
+            ThemeViewModel::class.java
+        )
+        mainViewModel.getThemeSettings().observe(this,
+            { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    switchTheme.isChecked = true
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    switchTheme.isChecked = false
+                }
+            })
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            mainViewModel.saveThemeSetting(isChecked)
+        }
+    }
+}
---
+++ app/src/main/java/com/yulius/warasapp/ui/profile/setting/ThemeViewModel.kt
@@ -0,0 +1,19 @@
+package com.yulius.warasapp.ui.profile.setting
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class ThemeViewModel(private val pref: SettingPreferences) : ViewModel() {
+    fun getThemeSettings(): LiveData<Boolean> {
+        return pref.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSetting(isDarkModeActive: Boolean) {
+        viewModelScope.launch {
+            pref.saveThemeSetting(isDarkModeActive)
+        }
+    }
+}
