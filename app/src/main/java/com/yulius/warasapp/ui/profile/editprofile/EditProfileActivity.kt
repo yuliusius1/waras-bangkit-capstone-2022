b04c9900f593108151e7f0f394ffc7d7df48ac7f
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-17 09:30:58 +0700

    Update EditProfileActivity.kt

--- app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
@@ -15,22 +15,50 @@ import com.yulius.warasapp.databinding.ActivityEditProfileBinding
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
+    var calender = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                calender.set(Calendar.YEAR, year)
+                calender.set(Calendar.MONTH, monthOfYear)
+                calender.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        binding.dateEditText.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@EditProfileActivity,
+                    dateSetListener,
+                    calender.get(Calendar.YEAR),
+                    calender.get(Calendar.MONTH),
+                    calender.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+        })
+        
         binding.btnGallery.setOnClickListener {
             startGallery()
         }
+    }
+    
+    private fun updateDateInView() {
+        val myFormat = getString(R.string.date_formate)
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        binding.dateEditText.setText(sdf.format(calender.getTime()))
     }
     
     private fun startGallery() {
         val intent = Intent()
         intent.action = Intent.ACTION_GET_CONTENT
         intent.type = "image/*"
-        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        val chooser = Intent.createChooser(intent, getString(R.string.choose))
         launcherIntentGallery.launch(chooser)
     }
 
