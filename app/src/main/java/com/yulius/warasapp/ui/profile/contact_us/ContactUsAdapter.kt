eb1fe657ebedde8682d047f801a16253712eaeb6
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-20 10:42:01 +0700

    Update ContactUsAdapter.kt

--- app/src/main/java/com/yulius/warasapp/ui/profile/contact_us/ContactUsAdapter.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/contact_us/ContactUsAdapter.kt
@@ -7,6 +7,12 @@ import com.yulius.warasapp.databinding.ListContactBinding
 import java.util.ArrayList
 
 class ListContactAdapter(private val listContact: ArrayList<Contact>) : RecyclerView.Adapter<ListContactAdapter.ListViewHolder>() {
+    
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -21,10 +27,16 @@ class ListContactAdapter(private val listContact: ArrayList<Contact>) : Recycler
         holder.binding.imgItemPhoto.setImageResource(photo)
         holder.binding.tvItemName.text = name
         holder.binding.tvItemEmail.text = email
+        
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listContact[holder.adapterPosition]) }
 
     }
 
     override fun getItemCount(): Int = listContact.size
+    
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Contact)
+    }
 
     class ListViewHolder(var binding: ListContactBinding) : RecyclerView.ViewHolder(binding.root) {
     }
