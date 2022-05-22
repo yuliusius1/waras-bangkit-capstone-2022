fb12ad199dc78801b6d729b2367b1cb4fb16a28b
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-22 13:28:07 +0700

    Update LandingActivity.kt

--- app/src/main/java/com/yulius/warasapp/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/landing/LandingActivity.kt
@@ -1,12 +1,45 @@
 package com.yulius.warasapp.ui.landing
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yulius.warasapp.R
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.yulius.warasapp.databinding.ActivityLandingBinding
+import com.yulius.warasapp.ui.auth.login.LoginActivity
 
 class LandingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLandingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+
+        binding.startBtn.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@LandingActivity,
+                    LoginActivity::class.java
+                )
+            )
+        }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
