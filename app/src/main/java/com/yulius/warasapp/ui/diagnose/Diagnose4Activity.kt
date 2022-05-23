8c52579e6653aa48ce5a650ac48a1fa9aafe6cd5
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-05-23 19:53:55 +0700

    Add files via upload

---
+++ app/src/main/java/com/yulius/warasapp/ui/diagnose/Diagnose4Activity.kt
@@ -0,0 +1,12 @@
+package com.yulius.warasapp.ui.diagnose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yulius.warasapp.R
+
+class Diagnose4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diagnose4)
+    }
+}
