175038b308603263ad379eafeae21a5e98284863
Alextonio-Winatian <101455767+Alextonio-Winatian@users.noreply.github.com>
2022-05-29 13:38:12 +0700

    Updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,13 +3,15 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     const token = req.header("x-auth-token");
     if (!token){
-     return res.json({status: "Error", message : "Access denied, No token provided"});
+        res.status(401);
+        return res.json({status: "Error", message : "Access denied, No token provided"});
     } 
     try {
         const decoded = jwt.verify(token, "jwtPrivateKey");
         req.users = decoded;
     } catch {
-        return res.json({status: "Error"});
+        res.status(401);
+        return res.json({status: "Error", message: "Token Expire!"});
     }
 
     next();
