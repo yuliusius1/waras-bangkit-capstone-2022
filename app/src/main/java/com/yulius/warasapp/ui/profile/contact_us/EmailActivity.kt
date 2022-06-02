f57ba3ce3706649e2e0c4c0dfc6bea123370df27
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-06-02 11:54:08 +0700

    Update EmailActivity.kt

--- app/src/main/java/com/yulius/warasapp/ui/profile/contact_us/EmailActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/contact_us/EmailActivity.kt
@@ -25,4 +25,78 @@ class EmailActivity : AppCompatActivity() {
             startActivity(Intent.createChooser(intent, getString(R.string.send_email)))
         }
     }
+}import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.yulius.warasapp.R
+import com.yulius.warasapp.data.model.Contact
+import com.yulius.warasapp.data.model.UserPreference
+import com.yulius.warasapp.databinding.ActivityEmailBinding
+import com.yulius.warasapp.ui.auth.login.LoginActivity
+import com.yulius.warasapp.util.ViewModelFactory
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "settings"
+)
+class EmailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEmailBinding
+    private lateinit var viewModel: ContactUsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = intent.getParcelableExtra<Contact>("DATA")
+
+        binding.tvItemEmail.text = getString(R.string.email_to) + " " + data?.name
+        binding.emailBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(getString(R.string.mail_to), "${data?.email}", null))
+            startActivity(Intent.createChooser(intent, getString(R.string.send_email)))
+        }
+
+        setupView()
+        setupViewModel()
+        setupAction()
+    }
+
+    private fun setupView() {
+        supportActionBar?.hide()
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore))
+        )[ContactUsViewModel::class.java]
+
+        viewModel.getUser().observe(this){
+            if (!it.isLogin){
+                val i = Intent(this, LoginActivity::class.java)
+                i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(i)
+            }
+        }
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            ivAvatar.setOnClickListener{
+                onBackPressed()
+            }
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
 }
