a6e0093f969ef3c16d7bc5472e0551ad70ae895e
kartikaokta123 <80581906+kartikaokta123@users.noreply.github.com>
2022-06-02 12:07:17 +0700

    Update EmailActivity.kt

--- app/src/main/java/com/yulius/warasapp/ui/profile/contact_us/EmailActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/contact_us/EmailActivity.kt
@@ -1,31 +1,6 @@
 package com.yulius.warasapp.ui.profile.contact_us
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.yulius.warasapp.R
-import com.yulius.warasapp.data.model.Contact
-import com.yulius.warasapp.databinding.ActivityEmailBinding
-
-class EmailActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityEmailBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityEmailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val data = intent.getParcelableExtra<Contact>("DATA")
-
-        binding.tvItemEmail.text = getString(R.string.email_to) + " " + data?.name
-        binding.emailBtn.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(getString(R.string.mail_to), "${data?.email}", null))
-            startActivity(Intent.createChooser(intent, getString(R.string.send_email)))
-        }
-    }
-}import android.content.Context
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
