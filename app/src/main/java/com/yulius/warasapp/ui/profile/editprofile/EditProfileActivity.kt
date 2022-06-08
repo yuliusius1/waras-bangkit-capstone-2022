6702b4b7153a0b6bc109833876f02fcc79e9c188
Yulius <46050557+yuliusius1@users.noreply.github.com>
2022-06-08 15:16:31 +0700

    Update Profile Page

--- app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileActivity.kt
@@ -1,23 +1,13 @@
 package com.yulius.warasapp.ui.profile.editprofile
 
-import android.Manifest
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.text.TextUtils
 import android.view.View
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -30,7 +20,7 @@ import com.yulius.warasapp.ui.auth.login.LoginActivity
 import com.yulius.warasapp.ui.main.MainActivity
 import com.yulius.warasapp.util.ResponseCallback
 import com.yulius.warasapp.util.ViewModelFactory
-import java.io.File
+import com.yulius.warasapp.util.getAges
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -46,35 +36,14 @@ class EditProfileActivity : AppCompatActivity() {
     private lateinit var viewModel : EditProfileViewModel
     private lateinit var user : User
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (!allPermissionsGranted()) {
-                Toast.makeText(
-                    this,
-                    getString(R.string.no_permission),
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
-            }
-        }
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupView()
         setupViewModel()
+        setupView()
         setupAction()
     }
 
@@ -94,23 +63,9 @@ class EditProfileActivity : AppCompatActivity() {
                 ).show()
             }
 
-            btnGallery.setOnClickListener {
-                startGallery()
-            }
-
-            btnCamera.setOnClickListener {
-                if (!allPermissionsGranted()) {
-                    ActivityCompat.requestPermissions(
-                        this@EditProfileActivity,
-                        REQUIRED_PERMISSIONS,
-                        REQUEST_CODE_PERMISSIONS
-                    )
-                }
-                startTakePhoto()
-            }
-
             saveButton.setOnClickListener {
                 var isError = false
+
                 if (TextUtils.isEmpty(nameEditTextLayout.editText?.text)){
                     isError = true
                     nameEditTextLayout.editText?.error = getString(R.string.enter_name)
@@ -126,9 +81,24 @@ class EditProfileActivity : AppCompatActivity() {
                     dateEditTextLayout.editText?.error = getString(R.string.enter_date)
                 }
 
+                if(getAges(dateEditTextLayout.editText?.text.toString()) < 0){
+                    isError = true
+                    dateEditTextLayout.error = getString(R.string.wrong_date_of_birth)
+                }
+
                 if (TextUtils.isEmpty(telpEditTextLayout.editText?.text)){
                     isError = true
                     telpEditTextLayout.editText?.error = getString(R.string.enter_tel)
+                }
+
+                if(emailEditTextLayout.editText?.text.toString() != user.email){
+                    viewModel.checkEmail(emailEditTextLayout.editText?.text.toString(), object : ResponseCallback{
+                        override fun getCallback(msg: String, status: Boolean) {
+                            if(!status){
+                                isError = true
+                            }
+                        }
+                    })
                 }
 
                 if (!isError){
@@ -145,9 +115,9 @@ class EditProfileActivity : AppCompatActivity() {
                             }
                         }
                     )
+
                 }
             }
-
         }
     }
 
@@ -173,20 +143,21 @@ class EditProfileActivity : AppCompatActivity() {
                         gender.setSelection(1)
                     }
                 }
+
+                dateSetListener =
+                    DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                        calender.set(Calendar.YEAR, year)
+                        calender.set(Calendar.MONTH, monthOfYear)
+                        calender.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                        updateDateInView()
+                    }
             }
         }
     }
 
     private fun setupView() {
+        showLoading()
         supportActionBar?.hide()
-
-        dateSetListener =
-            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-                calender.set(Calendar.YEAR, year)
-                calender.set(Calendar.MONTH, monthOfYear)
-                calender.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                updateDateInView()
-            }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -199,54 +170,7 @@ class EditProfileActivity : AppCompatActivity() {
         val sdf = SimpleDateFormat(myFormat, Locale.US)
         binding.dateEditText.setText(sdf.format(calender.time))
     }
-    
-    private fun startGallery() {
-        val intent = Intent()
-        intent.action = Intent.ACTION_GET_CONTENT
-        intent.type = "image/*"
-        val chooser = Intent.createChooser(intent, getString(R.string.choose))
-        launcherIntentGallery.launch(chooser)
-    }
-    
-    private fun startTakePhoto() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        intent.resolveActivity(packageManager)
-        com.yulius.warasapp.util.createTempFile(application).also {
-            val photoURI: Uri = FileProvider.getUriForFile(
-                this@EditProfileActivity,
-                "com.yulius.warasapp",
-                it
-            )
-            currentPhotoPath = it.absolutePath
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-            launcherIntentCamera.launch(intent)
-        }
 
-    }
-
-
-    private val launcherIntentGallery = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == RESULT_OK) {
-            val selectedImg: Uri = result.data?.data as Uri
-//            val myFile = uriToFile(selectedImg, this@EditProfileActivity)
-            binding.imgPhoto.setImageURI(selectedImg)
-        }
-    }
-    
-    private lateinit var currentPhotoPath: String
-    private val launcherIntentCamera = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == RESULT_OK) {
-            val myFile = File(currentPhotoPath)
-
-            val result = BitmapFactory.decodeFile(myFile.path)
-
-            binding.imgPhoto.setImageBitmap(result)
-        }
-    }
 
     private fun showDialogs(msg:String,status: Boolean) {
         if (status) {
@@ -281,11 +205,6 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    companion object {
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 
 }
--- app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileViewModel.kt
+++ app/src/main/java/com/yulius/warasapp/ui/profile/editprofile/EditProfileViewModel.kt
@@ -2,6 +2,7 @@ package com.yulius.warasapp.ui.profile.editprofile
 
 import android.util.Log
 import androidx.lifecycle.*
+import com.yulius.warasapp.data.model.ResponseData
 import com.yulius.warasapp.data.model.ResponseUpdate
 import com.yulius.warasapp.data.model.User
 import com.yulius.warasapp.data.model.UserPreference
@@ -24,6 +25,48 @@ class EditProfileViewModel(private val pref: UserPreference) : ViewModel() {
         viewModelScope.launch {
             pref.saveUser(user)
         }
+    }
+
+    fun checkEmail(email: String, callback: ResponseCallback){
+        _isLoading.value = true
+        ApiConfig.getApiService().checkEmail(email).enqueue(object:
+            Callback<ResponseData> {
+            override fun onResponse(
+                call: Call<ResponseData>,
+                response: Response<ResponseData>
+            ) {
+                val responseBody = response.body()
+                if(response.isSuccessful && responseBody != null){
+                    if(responseBody.status == "Success" && responseBody.data != null){
+                        if (responseBody.message != null){
+                            callback.getCallback(responseBody.message, true)
+                        } else {
+                            callback.getCallback("Email found! Redirect to verification page", true)
+                        }
+                        _isLoading.value = false
+
+                    } else {
+                        if (responseBody.message != null){
+                            callback.getCallback(responseBody.message, false)
+                        } else {
+                            callback.getCallback("Email not found!", false)
+                        }
+                        _isLoading.value = false
+
+                    }
+                } else {
+                    callback.getCallback(response.message(), false)
+                    _isLoading.value = false
+
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseData>, t: Throwable) {
+                callback.getCallback(t.message.toString(), false)
+                _isLoading.value = false
+
+            }
+        })
     }
 
     fun updateUser(user: User, fullName: String, email: String, gender: String, telephone: String, date:String , callback: ResponseCallback){
